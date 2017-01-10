134422b7f3af5d48a9bf6d304fb3926b45d9bf85
Minh Phuong and sandro mijatovic <minh@>
2017-01-10 10:54:09 -0800

    Add day and night mode with JS

---
+++ JS/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+});
